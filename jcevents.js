856df08ef3e1d4eaf00d349f943c42d6e6233dde
md678685 <mathew678@hotmail.co.uk>
2016-12-31 14:01:57 +0000

    Add connect/disconnect methods

--- jcevents.js
+++ jcevents.js
@@ -2,7 +2,17 @@ const eris = justcord.eris;
 const config = justcord.config;
 const chat = justcord.chat;
 
+// Chat
 jcmp.events.Add("chat_message", (player, message) => {
     if (message.beginsWith("/")) return;
-    justcord.eris.createMessage(justcord.config.eris.id, `${player.name}: ${message}`);
+    eris.createMessage(config.eris.id, `${player.name}: ${message}`);
+});
+
+// JC3MP
+jcmp.events.Add('ClientConnected', client => {
+    eris.createMessage(config.eris.id, `${client.name} connected to the server.`);
+});
+
+jcmp.events.Add('ClientDisconnected', (client, reason) => {
+    eris.createMessage(config.eris.id, `${client.name} disconnected from the server.`);
 });
