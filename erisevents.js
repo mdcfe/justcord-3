635ea367c7ea50227e50e047c1b909123ecd7f3c
md678685 <mathew678@hotmail.co.uk>
2017-01-06 15:58:30 +0000

    Format and log messages to console

--- erisevents.js
+++ erisevents.js
@@ -3,5 +3,8 @@ const config = justcord.config;
 const chat = justcord.chat;
 
 eris.on("messageCreate", (message) => {
-    chat.broadcast(message.content);
+    if (message.channel.id == config.eris.id) {
+        chat.broadcast(`${message.member.nick}: ${message.content}`);
+        console.log(`Discord: ${message.member.nick}: ${message.content}`);
+    }
 });
