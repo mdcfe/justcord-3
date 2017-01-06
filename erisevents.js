9232098c2653e90464b0928fef930bdc13e77540
md678685 <mathew678@hotmail.co.uk>
2017-01-06 16:10:44 +0000

    Ensure Justcord messages aren't relayed to chat

--- erisevents.js
+++ erisevents.js
@@ -8,7 +8,7 @@ eris.on("ready", () => {
 });
 
 eris.on("messageCreate", (message) => {
-    if (message.channel.id == config.eris.id) {
+    if (message.channel.id == config.eris.id && message.member.id != eris.user.id) {
         chat.broadcast(`${message.member.nick}: ${message.content}`);
         console.log(`Discord: ${message.member.nick}: ${message.content}`);
     }
