50a2ced1435bc43caa6f4d5cb101045672037756
md678685 <mathew678@hotmail.co.uk>
2017-01-07 21:47:35 +0000

    Allow customisation of game

--- erisevents.js
+++ erisevents.js
@@ -13,13 +13,14 @@ function formatFromDiscord(format, _message) {
         channel: _message.channel.name || "Unknown channel",
         message: _message.content,
         _message
-    }
+    };
     return evalTemplate(format, scope);
 }
 
 eris.on("ready", () => {
     console.log("Justcord ready!"); // TODO: Add logging utility functions
-    eris.createMessage(config.eris.id, "Server connected to the guild successfully!")
+    eris.createMessage(config.eris.id, "Server connected to the guild successfully!");
+    eris.editStatus("online", { name: config.eris.playing });
 });
 
 eris.on("messageCreate", (_message) => {
