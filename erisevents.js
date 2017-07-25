f5f10c55730ef4455e4aec6d438ea71ce9b8c2fe
md678685 <mathew678@hotmail.co.uk>
2017-07-25 15:13:39 +0100

    Use new Eris connect/disconnect messages and logging system

--- erisevents.js
+++ erisevents.js
@@ -5,6 +5,9 @@ const hexToRGB = util.hexToRGB;
 const eris = justcord.eris;
 const config = justcord.config;
 const chat = justcord.chat;
+const log = justcord.log;
+
+let firstConnect = true;
 
 function formatFromDiscord(format, _message) {
     const scope = {
@@ -28,14 +31,14 @@ function formatTopic() {
 
 function setTopic(topic) {
     eris.editChannel(config.eris.id, { topic }).catch((reason) => {
-        console.log(`Could not update topic (reason: ${reason})`);
+        log(`Could not update topic (reason: ${reason})`);
     });
 }
 
 eris.on("ready", () => {
-    console.log("Justcord ready!"); // TODO: Add logging utility functions
-    eris.createMessage(config.eris.id, "Server connected to the guild successfully!").catch((reason) => {
-        console.log(`Could not send connection message (reason: ${reason})`);
+    log("Connected to Discord.")
+    eris.createMessage(config.eris.id, (firstConnect ? config.gameToDiscord.dConnect : config.gameToDiscord.dReconnect)).catch((reason) => {
+        log(`Could not send connection message (reason: ${reason})`);
     });
     eris.editStatus("online", { name: config.eris.playing });
 
@@ -51,6 +54,11 @@ eris.on("messageCreate", (_message) => {
     if (_message.channel.id === config.eris.id && _message.member.id !== eris.user.id) {
         const message = formatFromDiscord(config.formatting.discordToGame.chat, _message);
         chat.broadcast(message, hexToRGB(config.formatting.discordToGame.colour));
-        console.log(`Discord: ${message}`);
+        log(`Discord: ${message}`);
     }
+});
+
+process.on("exit", () => {
+    eris.createMessage(config.eris.id, config.gameToDiscord.dExit).catch(() => log("wat"));
+    eris.editStatus("dnd", { name: "JC3MP server shutting down..." });
 });
